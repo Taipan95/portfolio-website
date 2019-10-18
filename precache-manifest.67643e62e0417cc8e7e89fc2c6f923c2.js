5ccc830e80ed8a57fb76a7aeeaf0517e804b150a
Sakis <sakis_95@live.com>
2019-10-18 16:00:40 +0300

    Updates

--- precache-manifest.67643e62e0417cc8e7e89fc2c6f923c2.js
+++ precache-manifest.67643e62e0417cc8e7e89fc2c6f923c2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "58b9be4d00fc0a2577184a9c0d9e3d6d",
+    "revision": "0529c75fb37c5ce82e75e177a35addce",
     "url": "/portfolio-website/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-website/static/css/2.04aae6c4.chunk.css"
   },
   {
-    "revision": "ba3d1af4054db53af82e",
-    "url": "/portfolio-website/static/css/main.b510133c.chunk.css"
+    "revision": "add640fe94d3eddd18f4",
+    "url": "/portfolio-website/static/css/main.c6eed4dc.chunk.css"
   },
   {
     "revision": "3ed1b8f06954ce2087ec",
     "url": "/portfolio-website/static/js/2.884c943c.chunk.js"
   },
   {
-    "revision": "ba3d1af4054db53af82e",
-    "url": "/portfolio-website/static/js/main.14c14c01.chunk.js"
+    "revision": "add640fe94d3eddd18f4",
+    "url": "/portfolio-website/static/js/main.accc5663.chunk.js"
   },
   {
     "revision": "73914b362ee78d003ca1",
