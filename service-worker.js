1d936485ced69830b3e63ab016bd1d53b9821c5e
Sakis <sakis_95@live.com>
2019-10-18 21:59:48 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-website/precache-manifest.f443a1520cab7b6150f800de067afba3.js"
+  "/portfolio-website/precache-manifest.3a64d2d160f8f3adcd819b7837be7c3a.js"
 );
 
 self.addEventListener('message', (event) => {
