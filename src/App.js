09186669b22029aa8023b3a8009bd2a3e9b1f0e3
Sakis <sakis_95@live.com>
2019-10-19 20:57:05 +0300

    removed contact me page/links

--- src/App.js
+++ src/App.js
@@ -7,30 +7,31 @@ import {Link} from 'react-router-dom';
 class App extends React.Component{
   render(){
     return (
-      <div className="demo-big-content">
-    <Layout>
-        <Header className="header-color" title="Sakis Evangelou" scroll>
-            <Navigation>
-                <Link to="/portfolio-website/resume">Resume</Link>
-                <Link to="/portfolio-website/aboutme">About Me</Link>
-                <Link to="/portfolio-website/projects">Projects</Link>
-                <Link to="/portfolio-website/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title="Title">
-            <Navigation>
-                <Link to="/portfolio-website/resume">Resume</Link>
-                <Link to="/portfolio-website/aboutme">About Me</Link>
-                <Link to="/portfolio-website/projects">Projects</Link>
-                <Link to="/portfolio-website/contact">Contact</Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main/>
-        </Content>
-    </Layout>
-</div>
+        <div className="demo-big-content">
+            <Layout>
+                <Header className="header-color" title={<a style={{	color: 'white',
+            textDecoration: 'none'}} href="/portfolio-website/">Sakis Evangelou</a>} scroll>
+                    <Navigation>
+                        <Link to="/portfolio-website/">Home</Link>
+                        <Link to="/portfolio-website/projects">Projects</Link>
+                        <Link to="/portfolio-website/resume">Resume</Link>
+                        <Link to="/portfolio-website/aboutme">About Me</Link>  
+                    </Navigation>
+                </Header>
+                <Drawer className="drawer-color">
+                    <Navigation>
+                    <Link to="/portfolio-website/">Home</Link>
+                        <Link to="/portfolio-website/projects">Projects</Link>
+                        <Link to="/portfolio-website/resume">Resume</Link>
+                        <Link to="/portfolio-website/aboutme">About Me</Link>  
+                    </Navigation>
+                </Drawer>
+                <Content>
+                    <div className="page-content" />
+                    <Main/>
+                </Content>
+            </Layout>
+        </div>
     );
   }
 }
